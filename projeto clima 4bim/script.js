ab014577e61420a6f548d8b9d1fec2df3240ebef
carolSu08 <carolineesuzart@gmail.com>
2025-10-10 11:58:30 -0300

    Add files via upload

---
+++ projeto clima 4bim/script.js
@@ -0,0 +1,39 @@
+document.querySelector('.busca').addEventListener('submit', async(event)=>{
+    event.preventDefault()
+
+    let input = document.querySelector('#searchInput').value
+
+    if(input !== ""){
+        showWarning("carregando...")
+
+        let results = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=
+           ${encodeURI(input)} &units=metrics&lang=lang=pt_br&appid=d06cdb298fafc83c520d5ab677fc477e`);
+           let json = await results.json();
+
+        if(json.cod ==200){
+            ShowInfo({
+                name:json.name,
+                country:json.sys.country,
+                temp:json.main.temp,
+                tempIcon: json.weather[0].icon,
+                windSpeed: json.wind.speed,
+                windAngle: json.deg
+
+        })
+
+        }else{
+            showWarning("Nao encontramos essa localização...")
+        } 
+
+    }else{
+
+    }
+})
+
+function showWarning(msg){
+    document.querySelector('.aviso').innerHTML = msg
+}
+
+function ShowInfo(json){
+
+}
